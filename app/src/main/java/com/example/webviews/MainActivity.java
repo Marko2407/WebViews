da01432b2c79dae03915f60806425abd54b45b32
Marko Vukosav <marko.vukosav85@gmail.com>
2021-05-09 00:45:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/webviews/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.webviews;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient()); //saying android that we wants the web to open on app
+
+        webView.loadUrl("https://mbesednik.github.io/Izrada-eCommerce-website/index.html");
+
+        // webView.loadData("<html><body><h2>Ja sam Marko</h2></body></htmlY", "text/html", "UTF-8");
+
+    }
+}
